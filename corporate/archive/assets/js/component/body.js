ac995e9594e5d4dbbe8d793f802f0bbbf4edf282
DhitoHesa <radhityohernanda@gmail.com>
2024-03-07 12:58:38 +0700

    updt

---
+++ corporate/archive/assets/js/component/body.js
@@ -0,0 +1,431 @@
+class Advance extends HTMLElement {
+  constructor() {
+    super();
+  }
+  connectedCallback() {
+    this.innerHTML = `
+    <!-- Start of Price section ============================================= -->
+    <section id="in-price" class="in-price-section">
+      <div class="container">
+        <div
+          class="in-section-title-2 in-title-wrap text-center headline pera-content"
+        >
+          <div class="sub-title text-uppercase">Pricing Plan</div>
+          <h2>Start Mulai Harga</h2>
+          <p>
+            Idealnya kelas berjalan secara bulanan untuk memantau hasil yang optimal dan perkembangan peserta secara berkala.
+          </p>
+        </div>
+        <div class="in-price-content">
+          <div class="row justify-content-center">
+            <div class="col-lg-4 col-md-6">
+              <div class="in-price-inner-items text-center">
+                <div class="pricing-title-price-area headline">
+                  <h2>BRONZE</h2>
+                  <h3><sup>RP.</sup>8.975.000</h3>
+                  <span>Per Month / PAX</span>
+                </div>
+                <div class="pricing-list-btn position-relative">
+                  <div class="pricing-list ul-li-block">
+                    <ul>
+                      <li>4x Pertemuan (MAX)</li>
+                      <li>2 Material (MAX)</li>
+                      <li>Offline / Online</li>
+                      <li>Exclude Transport</li>
+                      <li>Free Practice Class 1 Month</li>
+                    </ul>
+                  </div>
+                  <div class="price-btn">
+                    <div class="in-btn-1">
+                      <a href="#">Choose Plan</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+              <div class="in-price-inner-items text-center">
+                <div class="pricing-title-price-area headline">
+                  <h2>SILVER</h2>
+                  <h3><sup>RP.</sup>5.798.000</h3>
+                  <span>Per Month / PAX</span>
+                </div>
+                <div class="pricing-list-btn position-relative">
+                  <div class="pricing-list ul-li-block">
+                    <ul>
+                      <li>8x Pertemuan (MAX)</li>
+                      <li>4 Material (MAX)</li>
+                      <li>Offline / Online</li>
+                      <li>Exclude Transport</li>
+                      <li>Free Practice Class 2 Month</li>
+                    </ul>
+                  </div>
+                  <div class="price-btn">
+                    <div class="in-btn-1">
+                      <a href="#">Choose Plan</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+              <div class="in-price-inner-items text-center">
+                <div class="pricing-title-price-area headline">
+                  <h2>GOLDEN</h2>
+                  <h3><sup>RP.</sup>4.589.000</h3>
+                  <span>Per Month / PAX</span>
+                </div>
+                <div class="pricing-list-btn position-relative">
+                  <div class="pricing-list ul-li-block">
+                    <ul>
+                      <li>12x Pertemuan (MAX)</li>
+                      <li>4 Material (MAX)</li>
+                      <li>Offline / Online</li>
+                      <li>Exclude Transport</li>
+                      <li>Free Practice Class 3 Month</li>
+                    </ul>
+                  </div>
+                  <div class="price-btn">
+                    <div class="in-btn-1">
+                      <a href="#">Choose Plan</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- End of Price section -->
+    `;
+  }
+}
+
+class Basic extends HTMLElement {
+  constructor() {
+    super();
+  }
+  connectedCallback() {
+    this.innerHTML = `
+    <!-- Start of Price section ============================================= -->
+    <section id="in-price" class="in-price-section">
+      <div class="container">
+        <div
+          class="in-section-title-2 in-title-wrap text-center headline pera-content"
+        >
+          <div class="sub-title text-uppercase">Pricing Plan</div>
+          <h2>Start Mulai Harga</h2>
+          <p>
+            Idealnya kelas berjalan secara bulanan untuk memantau hasil yang
+            optimal dan perkembangan peserta secara berkala.
+          </p>
+        </div>
+        <div class="in-price-content">
+          <div class="row justify-content-center">
+            <div class="col-lg-4 col-md-6">
+              <div class="in-price-inner-items text-center">
+                <div class="pricing-title-price-area headline">
+                  <h2>STARTER</h2>
+                  <h3><sup>RP.</sup>7.975.000</h3>
+                  <span>Per Month / PAX</span>
+                </div>
+                <div class="pricing-list-btn position-relative">
+                  <div class="pricing-list ul-li-block">
+                    <ul>
+                      <li>4x Pertemuan (MAX)</li>
+                      <li>2 Material (MAX)</li>
+                      <li>Offline / Online</li>
+                      <li>Exclude Transport</li>
+                      <li>Free Practice Class 1 Month</li>
+                    </ul>
+                  </div>
+                  <div class="price-btn">
+                    <div class="in-btn-1">
+                      <a href="#">Choose Plan</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+              <div class="in-price-inner-items text-center">
+                <div class="pricing-title-price-area headline">
+                  <h2>PREMIUM</h2>
+                  <h3><sup>RP.</sup>4.798.000</h3>
+                  <span>Per Month / PAX</span>
+                </div>
+                <div class="pricing-list-btn position-relative">
+                  <div class="pricing-list ul-li-block">
+                    <ul>
+                      <li>8x Pertemuan (MAX)</li>
+                      <li>4 Material (MAX)</li>
+                      <li>Offline / Online</li>
+                      <li>Exclude Transport</li>
+                      <li>Free Practice Class 2 Month</li>
+                    </ul>
+                  </div>
+                  <div class="price-btn">
+                    <div class="in-btn-1">
+                      <a href="#">Choose Plan</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+              <div class="in-price-inner-items text-center">
+                <div class="pricing-title-price-area headline">
+                  <h2>ULTRA</h2>
+                  <h3><sup>RP.</sup>3.589.000</h3>
+                  <span>Per Month / PAX</span>
+                </div>
+                <div class="pricing-list-btn position-relative">
+                  <div class="pricing-list ul-li-block">
+                    <ul>
+                      <li>12x Pertemuan (MAX)</li>
+                      <li>4 Material (MAX)</li>
+                      <li>Offline / Online</li>
+                      <li>Exclude Transport</li>
+                      <li>Free Practice Class 3 Month</li>
+                    </ul>
+                  </div>
+                  <div class="price-btn">
+                    <div class="in-btn-1">
+                      <a href="#">Choose Plan</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- End of Price section -->
+    `;
+  }
+}
+
+class Sponsor extends HTMLElement {
+  constructor() {
+    super();
+  }
+  connectedCallback() {
+    this.innerHTML = `
+    <section id="in-sponsor" class="in-sponsor-section-2" style="margin-bottom: -65px;">
+		<div class="container">
+			<div class="in-sponsor-slider">
+				<div class="in-sponsor-item">
+					<div class="inner-img">
+						<img src="assets/img/corporate/uad.png" alt="">
+					</div>
+				</div>
+				<div class="in-sponsor-item">
+					<div class="inner-img">
+						<img src="assets/img/corporate/uin-suka.png" alt="">
+					</div>
+				</div>
+				<div class="in-sponsor-item">
+					<div class="inner-img">
+						<img src="assets/img/corporate/aveon.png" alt="">
+					</div>
+				</div>
+				<div class="in-sponsor-item">
+					<div class="inner-img">
+						<img src="assets/img/corporate/smk-dr-sutomo-temanggung.png" alt="">
+					</div>
+				</div>
+				<div class="in-sponsor-item">
+					<div class="inner-img">
+						<img src="assets/img/corporate/polita.png" alt="">
+					</div>
+				</div>
+				<div class="in-sponsor-item">
+					<div class="inner-img">
+						<img src="assets/img/corporate/mercu.png" alt="">
+					</div>
+				</div>
+				<div class="in-sponsor-item">
+					<div class="inner-img">
+						<img src="assets/img/corporate/amikom.png" alt="">
+					</div>
+				</div>
+				<div class="in-sponsor-item">
+					<div class="inner-img">
+						<img src="assets/img/corporate/ugm.png" alt="">
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+    `;
+  }
+}
+
+class Whychoose extends HTMLElement {
+  constructor() {
+    super();
+  }
+  connectedCallback() {
+    this.innerHTML = `
+<section id="in-why-choose-2" class="in-why-choose-section-2" data-background="assets/img/bg/cta.jpg">
+<div class="container">
+  <div class="in-portfolio-top-content-2 d-flex flex-wrap align-items-center justify-content-between">
+    <div class="in-section-title-2 headline wow fadeInLeft" data-wow-delay="200ms"
+      data-wow-duration="1000ms">
+      <div class="sub-title text-uppercase">
+        Why Choose
+      </div>
+      <h2>Pelatihan Di Dialogika</h2>
+    </div>
+    <div class="in-portfolio-title-text wow fadeInRight" data-wow-delay="400ms" data-wow-duration="1000ms">
+      Apabila Anda ragu untuk memulai In House Training dengan Dialogika, Anda bisa mencoba <b>trial secara gratis</b>. Syarat dan ketentuan bisa langsung chat tim kami.
+    </div>
+  </div>
+  <div class="in-why-choose-content-2">
+    <div class="row">
+      <div class="col-lg-6">
+        <div class="in-why-choose-img-text">
+          <div class="in-why-choose-img-area">
+            <div class="row">
+              <div class="col-md-6 wow fadeInUp" data-wow-delay="400ms"
+                data-wow-duration="1000ms">
+                <div class="in-why-choose-img">
+                  <img src="https://media.istockphoto.com/id/1128967659/id/foto/pelatih-asia-yang-serius-berbicara-di-pertemuan-grup-perusahaan-yang-beragam.jpg?s=612x612&w=0&k=20&c=Z7hynsbScKEgtio2K9pINiAGIN2wnGRhoLabPE8d-Wo=" alt="" style="height: 150px;">
+                </div>
+                <div class="in-why-choose-text-wrap">
+                  <div class="in-why-choose-text d-flex align-items-center">
+                    <div class="inner-icon">
+                      <img src="assets/img/about/wc-icon.png" alt="">
+                    </div>
+                    <div class="inner-text">
+                      Virtual & Face to Face
+                    </div>
+                  </div>
+                  <div class="in-why-choose-text d-flex align-items-center">
+                    <div class="inner-icon">
+                      <img src="assets/img/about/wc-icon.png" alt="">
+                    </div>
+                    <div class="inner-text">
+                      Flexible Payment
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-6 wow fadeInUp" data-wow-delay="500ms"
+                data-wow-duration="1000ms">
+                <div class="in-why-choose-img">
+                  <img src="https://media.istockphoto.com/id/1179434300/id/foto/pengusaha-india-memimpin-seminar.jpg?s=612x612&w=0&k=20&c=n2oKZuIeQubXAcH75oWu97-F0jz8x237zZVlLeLvMs8=" alt="" style="height: 150px;">
+                </div>
+                <div class="in-why-choose-text-wrap">
+                  <div class="in-why-choose-text d-flex align-items-center">
+                    <div class="inner-icon">
+                      <img src="assets/img/about/wc-icon.png" alt="">
+                    </div>
+                    <div class="inner-text">
+                      Free Class After
+                    </div>
+                  </div>
+                  <div class="in-why-choose-text d-flex align-items-center">
+                    <div class="inner-icon">
+                      <img src="assets/img/about/wc-icon.png" alt="">
+                    </div>
+                    <div class="inner-text">
+                      Exclusive Alumni
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="in-btn-2 position-relative wow fadeInUp" data-wow-delay="600ms"
+            data-wow-duration="1000ms">
+            <a href="#">Get Started Now</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-6">
+        <div class="in-why-choose-faq">
+          <div class="accordion" id="accordionExample2">
+            <div class="accordion-item headline-2 pera-content wow fadeInUp" data-wow-delay="200ms"
+              data-wow-duration="1500ms">
+              <h2 class="accordion-header" id="headingOne2">
+                <button class="accordion-button" type="button" data-bs-toggle="collapse"
+                  data-bs-target="#collapseOne2" aria-expanded="true"
+                  aria-controls="collapseOne">
+                  Berapakah Harga Pelatihannya?
+                </button>
+              </h2>
+              <div id="collapseOne2" class="accordion-collapse collapse show"
+                aria-labelledby="headingOne2" data-bs-parent="#accordionExample2">
+                <div class="accordion-body">
+                  Harga pelatihan sebenarnya beragam, harga yang kami cantumkan di website adalah <b>harga pada umumnya</b> namun di lapangan harga bisa jadi <b>lebih murah atau sebaliknya</b>, tergantung dengan kebutuhan, kerumitan, waktu pelaksanaan hingga ideal result.
+                </div>
+              </div>
+            </div>
+            <div class="accordion-item headline-2 pera-content wow fadeInUp" data-wow-delay="400ms"
+              data-wow-duration="1500ms">
+              <h2 class="accordion-header" id="headingTwo2">
+                <button class="accordion-button collapsed" type="button"
+                  data-bs-toggle="collapse" data-bs-target="#collapseTwo2"
+                  aria-expanded="false" aria-controls="collapseTwo2">
+                  Seperti Apa Waktu Pelaksanaannya?
+                </button>
+              </h2>
+              <div id="collapseTwo2" class="accordion-collapse collapse"
+                aria-labelledby="headingTwo2" data-bs-parent="#accordionExample2">
+                <div class="accordion-body">
+                  Waktu pelaksanaan bersifat kesepakatan bersama antara pihak Client dan Dialogika (mewakili mentor dan tim pelaksana). Idealnya seminggu 1x jika ingin mengambil pelatihan dalam waktu panjang. Namun juga sangat memungkin jika ingin dilaksanakan 3 hari berturut-turut dan seminggu full.
+                </div>
+              </div>
+            </div>
+            <div class="accordion-item headline-2 pera-content wow fadeInUp" data-wow-delay="600ms"
+              data-wow-duration="1500ms">
+              <h2 class="accordion-header" id="headingThree2">
+                <button class="accordion-button collapsed" type="button"
+                  data-bs-toggle="collapse" data-bs-target="#collapseThree2"
+                  aria-expanded="false" aria-controls="collapseThree2">
+                  Seperti apa kebijakan Free Trial?
+                </button>
+              </h2>
+              <div id="collapseThree2" class="accordion-collapse collapse"
+                aria-labelledby="headingThree2" data-bs-parent="#accordionExample2">
+                <div class="accordion-body">
+                  Free trial berlaku untuk kelas Basic Training Public Speaking yang meliputi: Mindset, Latihan Awal dan Umum Public Speaking hingga Pengenalan Presentasi. Pengisi Free Trial ditentukan sepihak dari Dialogika.<br><br>
+
+                  Apabila diluar kota jangkauan Dialogika, maka ada biaya akomodasi & transportasi yang harus dipenuhi.
+                </div>
+              </div>
+            </div>
+            <div class="accordion-item headline-2 pera-content wow fadeInUp" data-wow-delay="800ms"
+              data-wow-duration="1500ms">
+              <h2 class="accordion-header" id="headingFour">
+                <button class="accordion-button collapsed" type="button"
+                  data-bs-toggle="collapse" data-bs-target="#collapseFour"
+                  aria-expanded="false" aria-controls="collapseFour">
+                  Apakah ada Pembinaan Setelah Pelatihan?
+                </button>
+              </h2>
+              <div id="collapseFour" class="accordion-collapse collapse"
+                aria-labelledby="headingFour" data-bs-parent="#accordionExample2">
+                <div class="accordion-body">
+                  Idealnya ada, namun tergantung paket yang akan perusahaan ambil, apabila harga ditawar dengan sangat murah maka <b>bisa dipastikan pembinaan setelah pelatihan di tiadakan,</b> dan service dari kami bisa dipastikan tidak akan penuh dan optimal.
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</section>
+`;
+}
+}
+
+customElements.define("pricing-advance", Advance);
+customElements.define("pricing-basic", Basic);
+customElements.define("main-sponsor", Sponsor);
+customElements.define("main-choose", Whychoose);
